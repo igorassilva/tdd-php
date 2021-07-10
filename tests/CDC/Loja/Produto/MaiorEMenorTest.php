f4c1a0204d04b8e57d7f2d58fba0a87f09262ab4
Igor Augusto <igor.a.s.silva@gmail.com>
2021-07-10 13:15:52 -0300

    Finalizado o capítulo 2

---
+++ tests/CDC/Loja/Produto/MaiorEMenorTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace CDC\Loja\Produto;
+
+require "./vendor/autoload.php";
+
+use CDC\Loja\Carrinho\CarrinhoDeCompras,
+    CDC\Loja\Produto\Produto,
+    CDC\Loja\Produto\MaiorEMenor;
+
+use PHPUnit\Framework\TestCase as PHPUnit;
+
+class MaiorEMenorTest extends PHPUnit
+{
+    public function testOrdemDecrescente()
+    {
+        $carrinho = new CarrinhoDeCompras();
+
+        $carrinho->adiciona(new Produto("Geladeira", 450.00));
+        $carrinho->adiciona(new Produto("Liquidificador", 250.00));
+        $carrinho->adiciona(new Produto("Jogo de pratos", 70.00));
+
+        $maiorEMenor = new MaiorEMenor();
+        $maiorEMenor->encontra($carrinho);
+
+        $this->assertEquals("Jogo de pratos", $maiorEMenor->getMenor()->getNome());
+        $this->assertEquals("Geladeira", $maiorEMenor->getMaior()->getNome());
+    }
+
+    public function testApenasUmProduto()
+    {
+        $carrinho = new CarrinhoDeCompras();
+
+        $carrinho->adiciona(new Produto("Geladeira", 450.00));
+
+        $maiorEMenor = new MaiorEMenor();
+        $maiorEMenor->encontra($carrinho);
+
+        $this->assertEquals("Geladeira", $maiorEMenor->getMenor()->getNome());
+        $this->assertEquals("Geladeira", $maiorEMenor->getMaior()->getNome());
+
+        // $this->assertInstanceOf("CDC\Loja\Produto\Produto", $maiorEMenor->getMenor());
+        // $this->assertInternalType("object", $maiorEMenor->getMenor());
+    }
+}
